f5cb448fab9bb573a4bb6b3b75d0d7160e27f3de
epul2207 <epuls073@gmail.com>
2023-09-17 09:47:44 +0700

    Menambahkan file harga

--- lib/src/produk_baru.dart
+++ lib/src/produk_baru.dart
@@ -5,3 +5,11 @@ class Produk {
 extension Hello on Produk {
   void hello() => print('Nama produk adalah $nama_produk');
 }
+
+class Harga {
+  int harga_barang;
+
+  Harga(this.harga_barang);
+
+  void harga() => print('Harga nya adalah $harga_barang');
+}
